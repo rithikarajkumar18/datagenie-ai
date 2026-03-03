ce12b2afd68bce0b6bc5e4807d3c673d855861ce
rithikarajkumar18 <rithika.rajkumar18@gmail.com>
2026-03-03 13:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -328,28 +328,28 @@ def main_app():
 
         # prediction
     
-    if important_cols:
-         col = important_cols[0]
-         clean_df = df[[col]].dropna()
+        if important_cols:
+           col = important_cols[0]
+           clean_df = df[[col]].dropna()
 
-         if len(clean_df) > 1:
-            X = np.arange(len(clean_df)).reshape(-1, 1)
-            y = clean_df[col].values
-            model = LinearRegression().fit(X, y)
+          if len(clean_df) > 1:
+             X = np.arange(len(clean_df)).reshape(-1, 1)
+             y = clean_df[col].values
+             model = LinearRegression().fit(X, y)
+ 
+             pred = model.predict([[len(clean_df)]])[0]
 
-            pred = model.predict([[len(clean_df)]])[0]
+             st.success(f"Predicted next {col}: {pred:,.2f}")
+             insight_text += f"\nPredicted next {col}: {pred:,.2f}"
 
-            st.success(f"Predicted next {col}: {pred:,.2f}")
-            insight_text += f"\nPredicted next {col}: {pred:,.2f}"
-
-         else:
+          else:
             st.warning("Not enough clean data for prediction.")
-    else:
+        else:
          st.warning("No numeric business columns found for prediction.")
-    if st.button("Download AI Insights & Dashboard"):
-        pdf = create_full_pdf( insight_text,st.session_state.get("chart_path"))
+        if st.button("Download AI Insights & Dashboard"):
+           pdf = create_full_pdf( insight_text,st.session_state.get("chart_path"))
 
-        with open(pdf, "rb") as f:
+           with open(pdf, "rb") as f:
             st.download_button(
                 "Download PDF",
                 f,
@@ -374,6 +374,7 @@ if not st.session_state.logged_in:
         register_page()
 else:
     main_app()
+
 
 
 
